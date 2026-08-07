ec72d8ce0ec58e858a85b305f1935a5d01508441
25EC01003 <25ec01003@iitbbs.ac.in>
2026-08-07 10:33:44 +0530

    updated2

--- check_setup.py
+++ check_setup.py
@@ -8,8 +8,8 @@ print("numpy :", np. version )
 
 
 # a one-line smoke test of the plotting back-end
-plt.plot([0, 1, 2, 5], [0, 1, 4, 9], marker="o")
-plt.title("If you can see this window, the setup works")
+plt.plot([0, 1, 2, 5], [0, 1, 4, 15], marker="s",color="r")
+plt.title("diagram")
 plt.xlabel("x"); plt.ylabel("x squared")
 plt.grid(True)
 plt.show()
