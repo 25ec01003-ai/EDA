018d69101ddd7edd9fdc07811df0a9c9bd86f570
25EC01003 <25ec01003@iitbbs.ac.in>
2026-08-07 10:59:31 +0530

    made diagram into Diagram

--- check_setup.py
+++ check_setup.py
@@ -1,15 +1,15 @@
 import sys
 import numpy as np
 import pandas as pd
-import matplotlib
+
 import matplotlib.pyplot as plt
 print("Python :", sys.version.split()[0])
 print("numpy :", np. version )
 
 
 # a one-line smoke test of the plotting back-end
-plt.plot([0, 1, 2, 5], [0, 3, 4, 15], marker="s",color="r")
-plt.title("diagram")
+plt.plot([0, 1, 2, 5], [0, 9, 4, 15], marker="s",color="r")
+plt.title("Diagram")
 plt.xlabel("x"); plt.ylabel("x squared")
 plt.grid(True)
 plt.show()
