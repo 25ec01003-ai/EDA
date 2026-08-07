b4b5d5233c2579eaceda866e99275f0bd296a743
25EC01003 <25ec01003@iitbbs.ac.in>
2026-08-07 10:46:40 +0530

    updated 3

--- check_setup.py
+++ check_setup.py
@@ -8,7 +8,7 @@ print("numpy :", np. version )
 
 
 # a one-line smoke test of the plotting back-end
-plt.plot([0, 1, 2, 5], [0, 1, 4, 15], marker="s",color="r")
+plt.plot([0, 1, 2, 5], [0, 3, 4, 15], marker="s",color="r")
 plt.title("diagram")
 plt.xlabel("x"); plt.ylabel("x squared")
 plt.grid(True)
---
+++ readme.md
@@ -0,0 +1,3 @@
+* ec72d8c (HEAD -> master) updated2
+* f85b565 updated
+* a353c0c add repo
