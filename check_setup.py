d4bd7ae5f9e8dd101a700d84d61a6e9a7b2fa025
25EC01003 <25ec01003@iitbbs.ac.in>
2026-08-10 01:17:51 +0530

    E8 added

--- check_setup.py
+++ check_setup.py
@@ -1,19 +1,25 @@
-import sys
-import numpy as np
 import pandas as pd
-
+import numpy as np 
 import matplotlib.pyplot as plt
-print("Python :", sys.version.split()[0])
-print("numpy :", np. version )
+df=pd.read_csv("C:\EDA LAB\datasets\MOSFET_ID_VDS.csv")
 
-
-# a one-line smoke test of the plotting back-end
-plt.plot([0, 3, 2, 5], [0, 9, 4, 25], marker="s",color="r")
-plt.title("Diagram")
-<<<<<<< HEAD
-plt.xlabel("xnumbers"); plt.ylabel("jasanth squared")
-=======
-plt.xlabel("xnumbers"); plt.ylabel("x squared")
->>>>>>> conflict_w
-plt.grid(True)
+g_2d = np.gradient(df["I_D (2mA)"],df["V_DS (2V)"])
+g_3d = np.gradient(df["I_D (3mA)"],df["V_DS (3V)"])
+g_4d = np.gradient(df["I_D (4mA)"],df["V_DS (4V)"])
+g_5d = np.gradient(df["I_D (5mA)"],df["V_DS (5V)"])
+g_2d = np.nan_to_num(g_2d, nan=0)
+g_3d = np.nan_to_num(g_3d, nan=0)
+g_4d = np.nan_to_num(g_4d, nan=0)
+g_5d = np.nan_to_num(g_5d, nan=0)
+r_0 = (1/(1000*g_5d))*1000
+print(r_0)
+fig,ax=plt.subplots(1,figsize=(3,3),dpi=300)
+ax.plot(df["V_DS (2V)"],g_2d,color="r",label = "V_GS = 2V",linewidth=1,marker="s")
+ax.plot(df["V_DS (3V)"],g_3d,color="b",label = "V_GS = 3V",linewidth=1,marker="s")
+ax.plot(df["V_DS (4V)"],g_4d,color="y",label = "V_GS = 4V",linewidth=1,marker="s")
+ax.plot(df["V_DS (5V)"],g_5d,color="g",label = "V_GS = 5V",linewidth=1,marker="s")
+ax.set_xlabel("V_DS(V)")
+ax.set_ylabel("g_d(mA/V)")
+ax.set_title("g_d vs V_DS")
+ax.legend(loc="upper right")
 plt.show()
