d75323ac9c1c86a0a29eafc4be48a6a2b9109842
25EC01003 <25ec01003@iitbbs.ac.in>
2026-08-13 00:04:48 +0530

    added 2nd

--- check_setup.py
+++ check_setup.py
@@ -2,17 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df=pd.read_csv("C:\EDA LAB\datasets\MOSFET_ID_VDS.csv")
-
-fig,ax=plt.subplots(1,figsize=(3,3),dpi=300)
-ax.plot(df["V_DS (2V)"],df["I_D (2mA)"],color="r",label = "V_GS = 2V",linewidth=1)
-ax.plot(df["V_DS (3V)"],df["I_D (3mA)"],color="b",label = "V_GS = 3V",linewidth=1)
-ax.plot(df["V_DS (4V)"],df["I_D (4mA)"],color="y",label = "V_GS = 4V",linewidth=1)
-ax.plot(df["V_DS (5V)"],df["I_D (5mA)"],color="g",label = "V_GS = 5V",linewidth=1)
-ax.grid(linestyle="--",linewidth="0.5")
-ax.set_xlabel("V_DS(V)")
-ax.set_ylabel("I_D(mA)")
-ax.set_title("I_D vs V_DS")
-ax.legend(loc="upper left")
-
+df=pd.read_csv("C:\EDA LAB\datasets\MOSFET_ID_VGS.csv")
+fig,ax=plt.subplots(1,2,figsize=(3,3),dpi=300)
+ax[0].plot(df["V_GS (0.1V)"],df["I_D (0.1V)"],color="r",label = "V_DS = 0.1V",linewidth=1)
+ax[0].plot(df["V_GS (1V)"],df["I_D (1V)"],color="b",label = "V_DS = 1V",linewidth=1)
+ax[0].plot(df["V_GS (3V)"],df["I_D (3V)"],color="y",label = "V_DS = 3V",linewidth=1)
+ax[0].plot(df["V_GS (5V)"],df["I_D (5V)"],color="g",label = "V_DS = 5V",linewidth=1)
+ax[0].set_xlabel("V_GS(V)")
+ax[0].set_ylabel("I_D(mA)")
+ax[0].set_title("I_D vs V_GS")
+ax[0].legend(loc="upper left")
 plt.show()
