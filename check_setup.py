a270a2d1b3f8172feb7b1f451e71b57f704919cc
25EC01003 <25ec01003@iitbbs.ac.in>
2026-08-14 09:32:06 +0530

    official

--- check_setup.py
+++ check_setup.py
@@ -1,19 +1,18 @@
-import sys
-import numpy as np
 import pandas as pd
-
 import matplotlib.pyplot as plt
-print("Python :", sys.version.split()[0])
-print("numpy :", np. version )
 
 
-# a one-line smoke test of the plotting back-end
-plt.plot([0, 3, 2, 5], [0, 9, 4, 25], marker="s",color="r")
-plt.title("Diagram")
-<<<<<<< HEAD
-plt.xlabel("xnumbers"); plt.ylabel("jasanth squared")
-=======
-plt.xlabel("xnumbers"); plt.ylabel("x squared")
->>>>>>> conflict_w
-plt.grid(True)
+df=pd.read_csv("C:\EDA LAB\datasets\Diode_IV_Temperature.csv")
+
+fig,ax=plt.subplots(1,figsize=(3,3),dpi=350)
+ax.plot(df["V(0)"],df["I(mA)"],color="r",label = "T = 0C",linewidth=1)
+ax.plot(df["V(25)"],df["I(25)"],color="b",label = "T = 25C",linewidth=1)
+ax.plot(df["V(50)"],df["I(50)"],color="y",label = "T = 50C",linewidth=1)
+ax.plot(df["V(75)"],df["I(75)"],color="g",label = "T = 75C",linewidth=1)
+ax.grid(True,linestyle="--")
+ax.set_xlabel("Voltage(V)")
+ax.set_ylabel("current(mA)")
+ax.set_title("I - V Graph at diff Temp")
+ax.legend(loc="upper left")
+
 plt.show()
